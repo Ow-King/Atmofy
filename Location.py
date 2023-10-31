a5a46d743a8145b2bda3b4394f398fdc678008d4
Ow-King <owkington@gmail.com>
2023-10-30 21:05:02 -0400

    Added Date Specific Song storage Files

--- Location.py
+++ Location.py
@@ -1,17 +1,19 @@
 import requests
-from bs4 import BeautifulSoup
+import bs4
+import time
 
 class Location:
     def __init__(self, zip):
         self.zip = zip
 
+    @property
     def getWeather(self):
         # create url
         url = "https://www.google.com/search?q=" + "weather" + self.zip
         # requests instance
         html = requests.get(url).content
         # getting raw data
-        soup = BeautifulSoup(html, 'html.parser')
+        soup = bs4.BeautifulSoup(html, 'html.parser')
         temp = soup.find('div', attrs={'class': 'BNeawe iBp4i AP7Wnd'}).text
         str = soup.find('div', attrs={'class': 'BNeawe tAd8D AP7Wnd'}).text
 
@@ -24,11 +26,26 @@ class Location:
         temp = tempList[0]
         weekday = timeList[0]
         time = timeList[1] + " " + timeList[2]
-        # printing all data
 
+        # printing all data
         print("Temperature is", temp)
         print("Weekday is", weekday)
-        print("Time: ", time)
         print("Sky Description: ", sky)
 
-        return temp, weekday, time, sky
+        return temp, weekday, sky
+
+    def getTime(self):
+
+        t = time.localtime(time.time())
+
+        timeFrame = "unlisted"
+        if (t.tm_hour >= 5 & t.tm_hour < 11):
+            timeFrame = "Morning"
+        if (t.tm_hour >= 11 & t.tm_hour < 17):
+            timeFrame = "Midday"
+        if (t.tm_hour >= 17 & t.tm_hour < 23):
+            timeFrame = "Evening"
+        else:
+            timeFrame = "Night"
+
+        return ("songsPlayed" + timeFrame + ".txt")
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import spotipy
 import time
+
 from numpy.matlib import rand
 from spotipy.oauth2 import SpotifyOAuth
 
@@ -23,38 +24,45 @@ def main():
     weather = Location(zipCode)
     user = Atmofy(sp)
 
-    temp, weekday, clock, sky = weather.getWeather()
+    temp, weekday, sky = weather.getWeather
 
     # Takes the URI of the playlist and turns it into a DataFrame of Attributes to be accessed for cosine similarity
     playlist_URI = playlist_link.split("/")[-1].split("?")[0]
     track_uris = [x["track"]["uri"] for x in sp.playlist_tracks(playlist_URI)["items"]]
     tracks = []
 
-    for i in range(len(track_uris)):
-        track = user.get_track_features(track_uris[i])
-        tracks.append(track)
-    df = pd.DataFrame(tracks,
-                      columns=['name', 'album', 'artist', 'release_date', 'length', 'popularity', 'danceability',
-                               'acousticness', 'danceability', 'energy', 'instrumentalness', 'liveness', 'loudness',
-                               'speechiness', 'tempo', 'time_signature', 'URI'])
-    print(df.to_string())
 
-    # sp.add_to_queue(str(df.get('URI')[2]))
+    #   for i in range(len(track_uris)):
+    #       track = user.get_track_features(track_uris[i])
+    #       tracks.append(track)
+    #   df = pd.DataFrame(tracks,
+    #                     columns=['name', 'album', 'artist', 'release_date', 'length', 'popularity', 'danceability',
+    #                              'acousticness', 'danceability', 'energy', 'instrumentalness', 'liveness', 'loudness',
+    #                              'speechiness', 'tempo', 'time_signature', 'URI'])
+    #   print(df.to_string())
+    #   sp.add_to_queue(str(df.get('URI')[2]))
+
+    fileName = weather.getTime()
 
     artist, track, tid = user.get_current_song()
-    user.store_current_song("songsPayed.txt", tid, 0)
+    user.store_current_song(fileName, tid, 0)
 
     # Every 3 seconds check to see if the song has changed, if so store the new song ID into the file
-    timePlayed = 0;
+    timePlayed = time.time();
+    timePlayed = int(timePlayed * 1000)
 
     while True:
         artist, track, tid = user.get_current_song()
-        if (user.check_song_change("songsPayed.txt", tid)):
-            user.store_current_song("songsPayed.txt", tid, timePlayed)
+        if (user.check_song_change(fileName, tid)):
+            user.store_current_song(fileName, tid, int(time.time() * 1000) - timePlayed)
             print("NEW SONG DETECTED AND STORED")
 
+            # Update Current Time
+            fileName = weather.getTime()
+
             # Reset timer on how long song was played
-            timePlayed = 0;
+            timePlayed = time.time();
+            timePlayed = int(timePlayed * 1000)
         else:
             print("NO CHANGE IN SONG")
 
