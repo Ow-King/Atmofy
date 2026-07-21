f3a913f264422e57241ebf7281730bff312bb3cf
Ow-King <okington@purdue.edu>
2026-07-21 13:11:12 -0500

    auth testing

--- app/spotify/auth.py
+++ app/spotify/auth.py
@@ -13,3 +13,10 @@ def get_spotify_client():
                                                     client_secret=os.environ["SPOTIPY_CLIENT_SECRET"],
                                                     redirect_uri=os.environ["SPOTIPY_REDIRECT_URI"],
                                                     scope="user-read-currently-playing user-modify-playback-state"))
+    return sp
+
+# Test Auth
+if __name__ == "__main__":
+    client = get_spotify_client()
+    me = client.current_user()
+    print(f"Authenticated as: {me['display_name']} ({me['id']})")
